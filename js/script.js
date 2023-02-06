edb23b503e2019d3c32d793063dbd6121d29523c
Mateusz Leszczyński <bxrq@wp.pl>
2023-02-06 14:43:38 +0100

    tasks.css and script.js classes rename

--- js/script.js
+++ js/script.js
@@ -45,9 +45,9 @@
              <li 
              ${task.done ? "style=\"text-decoration: line-through\"" : ""}
              >
-             <button class="list__button--green js-done">✓</button>
+             <button class="tasks__button--green js-done">✓</button>
              ${task.content}
-             <button class="list__button--red js-remove">🗑</button>
+             <button class="tasks__button--red js-remove">🗑</button>
              </li>
              `;
         }
@@ -79,91 +79,3 @@
 
     init();
 }
-
-
-
-
-
-//code before refactoring
-
-// {
-//     const tasks = [];
-
-//     const addNewTask = (newTaskContent) => {
-//         tasks.push({
-//             content: newTaskContent,
-//         });
-
-//         render();
-//     };
-
-//     const removeTask = (taskIndex) => {
-//         tasks.splice(taskIndex, 1);
-//         render();
-//     };
-
-//     const toggleTaskDone = (taskIndex) => {
-//         tasks[taskIndex].done = !tasks[taskIndex].done;
-//         render();
-//     }
-
-//     const bindEvents = () => {
-//         const removeButtons = document.querySelectorAll(".js-remove");
-
-//         removeButtons.forEach((removeButton, index) => {
-//             removeButton.addEventListener("click", () => {
-//                 removeTask(index);
-//             });
-//         });
-
-//         const toggleDoneButtons = document.querySelectorAll(".js-done");
-
-//         toggleDoneButtons.forEach((toggleDoneButton, index) => {
-//             toggleDoneButton.addEventListener("click", () => {
-//                 toggleTaskDone(index);
-//             });
-//         });
-//     }
-
-//     const render = () => {
-//         let htmlString = "";
-
-//         for (const task of tasks) {
-//             htmlString += `
-//              <li 
-//              ${task.done ? "style=\"text-decoration: line-through\"" : ""}
-//              >
-//              <button class="js-done"><img src="https://a.slack-edge.com/production-standard-emoji-assets/14.0/google-large/2705.png"></button>
-//              ${task.content}
-//              <button class="js-remove"><img src="https://a.slack-edge.com/production-standard-emoji-assets/14.0/google-large/1f7e5.png"></button>
-//              </li>
-//              `;
-//         }
-
-//         document.querySelector(".js-tasks").innerHTML = htmlString;
-
-//         bindEvents();
-//     };
-
-//     const onFormSubmit = (event) => {
-//         event.preventDefault();
-
-//         const newTaskContent = document.querySelector(".js-newTask").value.trim();
-
-//         if (newTaskContent === "") {
-//             return;
-//         }
-
-//         addNewTask(newTaskContent);
-//     }
-
-//     const init = () => {
-//         render();
-
-//         const form = document.querySelector(".js-form");
-
-//         form.addEventListener("submit", onFormSubmit);
-//     };
-
-//     init();
-// }
